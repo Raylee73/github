50f22aab08b7bb8ca8a8b97177a7463cea0dc0c9
Raylee73 <reitumetseletwaba73@gmail.com>
2024-10-10 15:13:00 +0200

    second commit

---
+++ GITHUBCMD/src/githubcmd/GITHUBCMD.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubcmd;
+
+/**
+ *
+ * @author ANDRONICA KGOSI
+ */
+public class GITHUBCMD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("PUSHED FROM COMMAND PROMPT");
+    }
+    
+}
